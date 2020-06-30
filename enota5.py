f17dc3beadaf5b20f9286e04e2fc7edc0127cf07
Marco T <marcotts@gmail.com>
2020-06-30 10:18:12 +0200

    update

---
+++ enota5.py
@@ -0,0 +1,71 @@
+from utilities import WordType, WordNote, PronNote, BookPage, Level, Item, process_dictionary, find_random_answers, \
+    start_tests, Gender
+
+enota = {}
+
+# č š ž
+
+enota[5] = (
+
+    ("kakšno pico imate radi?", "che tipo di pizza vi piace?"),
+    ("kakšno pico imaš rad/a?", "che tipo di pizza ti piace?"),
+    ("kakšen sendvič imaš rad/a?", ""),
+    ("potem", "dopo"),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+    ("", ""),
+
+
+)
+
+
+
+start_tests(enota, int_seed=None)
+
+
