93294d5e61b5c4037cfac2424ccae2c0de2717b4
Marco T <marcotts@gmail.com>
2020-07-08 12:45:53 +0200

    update

--- README.md
+++ README.md
@@ -2,8 +2,35 @@
 
 utilities for learning Slovenian language (for italian speakers)
 
-asks to user to choose the correct italian translation to slovenian words or sentences.
+the program asks user to choose among the correct italian translation of slovenian words or sentences.
 
-maybe these programs can be more useful if used with this book:
+these program can be more useful if used while studying this book:
 
 https://centerslo.si/en/books/textbooks-and-handbooks/beginner-level/slovenscina-ekspres-1/
+
+examples:
+```
+item 76 - 1/249
+cosa vuol dire: 'junij' ?
+a : professione
+b : come
+c : agosto
+d : signore
+e : bene
+f : giugno
+risposta (q per uscire): f
+OK
+slovensko='junij' italiansko='giugno'
+
+item 37 - 2/249
+cosa vuol dire: 'lahko noč' ?
+a : come stai? (come sei?)
+b : cosa sei di mestiere/che lavoro fai?
+c : buona notte
+d : peter, sei laureato in economia?
+e : io ho 48 anni
+f : chi è questo?
+risposta (q per uscire): a
+NOT OK
+*** risposta corretta: buona notte
+```
--- main.py
+++ main.py
@@ -1,5 +1,53 @@
 import enota1
+import enota2
+import enota3
+import enota4
+import enota5
+import enota6
+import extra
+import numbers
+import time
+import verbs
 
+while 1:
 
-enota1.run_me()
+    print("a: enota 1")
+    print("b: enota 2")
+    # print("c: enota 3")
+    # print("d: enota 4")
+    print("e: enota 5")
+    print("f: enota 6")
+    print("g: extra")
+    print("h: numbers")
+    print("i: time")
+    print("l: verbs")
+    # print("z: tutti i test")
+    print("q: esci")
 
+    cmd = input("scegli il test:")
+
+    if cmd == 'q':
+        print("bye!")
+        exit(0)
+    elif cmd == 'a':
+        enota1.run_me()
+    elif cmd == 'b':
+        enota2.run_me()
+    elif cmd == 'c':
+        enota3.run_me()
+    elif cmd == 'd':
+        enota4.run_me()
+    elif cmd == 'e':
+        enota5.run_me()
+    elif cmd == 'f':
+        enota6.run_me()
+    elif cmd == 'g':
+        extra.run_me()
+    elif cmd == 'h':
+        numbers.run_me()
+    elif cmd == 'i':
+        time.run_me()
+    elif cmd == 'l':
+        verbs.run_me()
+    else:
+        print("non ho capito la scelta!")
