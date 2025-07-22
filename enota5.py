640d87050eb54b835ecb05be4c6aa68502df7ffc
marco <marcotts@gmail.com>
2025-07-22 13:37:31 +0200

    update

--- enota5.py
+++ enota5.py
@@ -1,5 +1,5 @@
 from utilities import WordType, WordNote, PronNote, BookPage, Level, Item, process_dictionary, find_random_answers, \
-    start_tests, Gender
+    start_tests, Gender, generic_run_me
 
 enota = {}
 
@@ -169,22 +169,6 @@ enota["enota 5"] = (
 
 )
 
-
-def run_me():
-    dict_slo, dict_ita = process_dictionary(enota)
-
-    print("1 - test da sloveno a italiano")
-    print("2 - test da italiano a sloveno")
-    data = input("risposta (q per uscire): ")
-    if data is None or data == "q":
-        return
-    elif data == "1":
-        start_tests(dict_slo)
-    elif data == "2":
-        start_tests(dict_ita, slo2ita=False)
-    else:
-        print("risposta non valida")
-
-
 if __name__ == "__main__":
-    run_me()
+    generic_run_me(enota)
+
